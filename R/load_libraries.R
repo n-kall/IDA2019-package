2c3357538bb72d4abaf7b8395f2b50512ec563a9
n-kall <noa@tutamail.com>
2019-11-01 10:05:42 +0100

    add load_libraries function

---
+++ R/load_libraries.R
@@ -0,0 +1,11 @@
+load_libraries()  <- function() {
+    suppressPackageStartupMessages(library(tidyverse))
+    suppressPackageStartupMessages(library(brms))
+    suppressPackageStartupMessages(library(ggsignif))
+    suppressPackageStartupMessages(library(gridExtra))
+    suppressPackageStartupMessages(library(Rlab))
+    suppressPackageStartupMessages(library(rcartocolor))
+    suppressPackageStartupMessages(library(extraDistr))
+    suppressPackageStartupMessages(library(boot))
+    suppressPackageStartupMessages(library(naniar))
+}

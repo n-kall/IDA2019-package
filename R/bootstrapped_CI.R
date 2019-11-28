dbd54ce1bbceac1919d6ad1b2e6a0ad72e703bb2
MarauderPixie <tobias.anton.85@gmail.com>
2019-11-28 16:44:46 +0100

    update DESCRIPTION, introduce bootstrap function

---
+++ R/bootstrapped_CI.R
@@ -0,0 +1,25 @@
+#' Produce bootstrapped 95% Confidence Interval
+#'
+#' @param data_vector observed data to sample from
+#' @param n_resamples number of resamples to take
+#'
+#' @return JSON
+#' @importFrom purrr map_dbl
+#' @importFrom dplyr tibble
+#' @export
+#' 
+bootstrapped_CI <-  function(data_vector, n_resamples = 1000) {
+  map_dbl(seq(n_resamples), function(i) {
+    mean(sample(x = data_vector, 
+                size = length(data_vector), 
+                replace = T)
+    )
+  }
+  
+  )
+  tibble(
+    'lower' = quantile(resampled_means, 0.025),
+    'mean'  = mean(data_vector),
+    'upper' = quantile(resampled_means, 0.975)
+  ) 
+}

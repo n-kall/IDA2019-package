74719b9a968bdc0cc2a7a777e7acaad3f7c6762e
MarauderPixie <tobias.anton.85@gmail.com>
2020-01-06 16:56:18 +0100

    expand options for drawing axes

--- R/theme_ida.R
+++ R/theme_ida.R
@@ -3,7 +3,7 @@
 #' @param title.size font size of all title elements + facet labels
 #' @param text.size font size of all text elements
 #' @param legend.position either one of "top", "bottom", "left" or "right"
-#' @param y.axis logical; show a solid y-axis?
+#' @param show.axis should axes be drawn? (TRUE, FALSE, "x", "y")
 #' @param plot.margin margin around entire plot (in cm of the top, right, bottom, and left margins)
 #'
 #' @importFrom ggplot2 theme
@@ -14,7 +14,7 @@
 #' @importFrom ggplot2 unit
 #' @export
 theme_ida <- function(title.size = 16, text.size = 14,
-                      legend.position = "top", y.axis = TRUE, 
+                      legend.position = "top", show.axis = FALSE, 
                       plot.margin = c(.2, .1, .2, .1)){
   # baseline
   layout <- ggplot2::theme_classic()
@@ -23,11 +23,20 @@ theme_ida <- function(title.size = 16, text.size = 14,
                                                 face = "bold"),
                            line = element_line(size = .5))
   
-  # axis
-  layout <- layout + theme(axis.line.x = element_blank())
-  
-  if (isFALSE(y.axis)){
-    layout <- layout + theme(axis.line.y = element_blank())
+  # axes
+  if (inherits(show.axis, "character") | show.axis == FALSE){
+    if (inherits(show.axis, "character")){
+      show.axis <- tolower(show.axis)
+      if (show.axis == "x"){
+        layout <- layout + theme(axis.line.y = element_blank())
+      }
+      if (show.axis == "y"){
+        layout <- layout + theme(axis.line.x = element_blank())
+      }
+    } else {
+      layout <- layout + theme(axis.line.x = element_blank(),
+                               axis.line.y = element_blank())
+    }
   }
   
   # legend
